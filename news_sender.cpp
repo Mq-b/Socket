16b61973f4f14f825e353bff9f305a4e39e373dd
mq白 <97590219+Mq-b@users.noreply.github.com>
2023-05-11 16:53:22 +0800

    完成Linux下广播的demo
    单播和广播是两个极端，要么一个，要么全部。
    多播提供一种折衷方案，多播数据报仅由对该数据报感兴趣的接口接收。
    广播一般局限于局域网。
    而多播既可以用于局域网也可以跨越广域网

---
+++ news_sender.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string.h>
+#include<unistd.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+constexpr size_t BUF_SIZE{1024};
+constexpr size_t TTL {64};
+
+void error_handling(const char*message){
+	fputs(message,stderr);
+	fputc('\n',stderr);
+	exit(1);
+}
+int main(int argc,char*argv[]){
+	if(argc!=3){
+		printf("Usage : %s <GroupIp> <PORT>\n",argv[0]);
+		exit(1);
+	}
+
+    int send_sock = socket(PF_INET,SOCK_DGRAM,0);
+    sockaddr_in mul_adr{};
+    mul_adr.sin_family=AF_INET;
+    mul_adr.sin_addr.s_addr=inet_addr(argv[1]);
+    mul_adr.sin_port=htons(static_cast<short>(atoi(argv[2])));
+
+    int time_live = TTL;
+    setsockopt(send_sock,SOL_SOCKET,SO_BROADCAST,&time_live,sizeof(time_live));
+
+    FILE*fp{};
+    if((fp=fopen("news.txt","r"))==nullptr)
+        error_handling("fopen() error");
+    
+    char buf[BUF_SIZE]{};
+    while(!feof(fp)){
+        fgets(buf,BUF_SIZE,fp);
+        sendto(send_sock,buf,BUF_SIZE,0,(sockaddr*)&mul_adr,sizeof(mul_adr));
+        sleep(2);
+    }
+    fclose(fp);
+    close(send_sock);
+}
